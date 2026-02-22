da35a89cdd1a117262da21f09001a322f464823f
Mallory Wittwer <mallory.wittwer@epfl.ch>
2026-02-22 18:10:19 +0100

    Make NapariResults inherit from Results and call post_ events

--- src/napari_serverkit/widgets/napari_results.py
+++ src/napari_serverkit/widgets/napari_results.py
@@ -11,7 +11,7 @@ import napari.layers
 from napari.utils.notifications import show_error, show_info, show_warning
 from qtpy.QtWidgets import QProgressBar
 
-from imaging_server_kit.core.results import Results, LayerStackBase, DataLayer
+from imaging_server_kit.core.results import Results, DataLayer
 
 
 def _set_layer_attributes_from_meta(meta: Dict, napari_layer: napari.layers.Layer):
@@ -31,38 +31,6 @@ def _set_layer_attributes_from_meta(meta: Dict, napari_layer: napari.layers.Laye
                 print("Could not set this layer property: ", key)
 
 
-def create(viewer, layer: DataLayer) -> None:
-    if layer.data is None:
-        return
-    
-    kind = layer.kind
-    data = layer.data
-    name = layer.name
-    meta = layer.meta
-    
-    napari_layer = None
-    if kind == "image":
-        napari_layer = viewer.add_image(data, name=name)
-    elif kind == "mask":
-        napari_layer = viewer.add_labels(data.astype(np.uint16), name=name)
-    elif kind == "points":
-        napari_layer = viewer.add_points(data, name=name)
-    elif kind in ["boxes", "paths"]:
-        if "shape_type" in meta:  # Make sure it isn't used twice
-            meta.pop("shape_type")
-        if kind == "boxes":
-            napari_layer = viewer.add_shapes(data, name=name, shape_type="rectangle")
-        elif kind == "paths":
-            napari_layer = viewer.add_shapes(data, name=name, shape_type="path")
-    elif kind == "vectors":
-        napari_layer = viewer.add_vectors(data, name=name)
-    elif kind == "tracks":
-        napari_layer = viewer.add_tracks(data, name=name)
-
-    if napari_layer is not None:
-        _set_layer_attributes_from_meta(meta, napari_layer)
-
-
 @dataclass
 class UpdateContext:
     viewer: napari.Viewer
@@ -71,15 +39,12 @@ class UpdateContext:
 
 
 def _napari_layer_update(ctx: UpdateContext):
-    if not ctx.layer.name in [l.name for l in ctx.viewer.layers]:
-        create(ctx.viewer, ctx.layer)
-    else:
-        for l in ctx.viewer.layers:
-            if l.name == ctx.layer.name:
-                if ctx.layer.data is not None:
-                    # Assign to the Napari layer data the data from the corresponding context layer
-                    l.data = ctx.layer.data
-                    _set_layer_attributes_from_meta(ctx.layer.meta, l)
+    for l in ctx.viewer.layers:
+        if l.name == ctx.layer.name:
+            if ctx.layer.data is not None:
+                # Assign to the Napari layer data the data from the corresponding context layer
+                l.data = ctx.layer.data
+                _set_layer_attributes_from_meta(ctx.layer.meta, l)
 
 
 def _notification_update(ctx: UpdateContext):
@@ -102,85 +67,19 @@ def _pbar_update(ctx: UpdateContext):
     if ctx.layer.data is not None:
         ctx.pbar.setValue(ctx.layer.data)
         ctx.pbar.setMaximum(ctx.layer.meta["max_val"])
-        
-
-def update(viewer, layer: Optional[DataLayer], pbar: QProgressBar) -> None:
-    """Based on the kind of layer, execute the right update function."""
-    if layer is None:
-        return
-
-    update_hooks = {
-        "image": _napari_layer_update,
-        "mask": _napari_layer_update,
-        "points": _napari_layer_update,
-        "boxes": _napari_layer_update,
-        "paths": _napari_layer_update,
-        "vectors": _napari_layer_update,
-        "tracks": _napari_layer_update,
-        "notification": _notification_update,
-        "float": _textlayer_update,
-        "int": _textlayer_update,
-        "bool": _textlayer_update,
-        "str": _textlayer_update,
-        "choice": _textlayer_update,
-        "progress": _pbar_update,
-    }
-    update_func: Optional[Callable] = update_hooks.get(layer.kind)
-    if update_func is not None:
-        ctx = UpdateContext(viewer=viewer, layer=layer, pbar=pbar)
-        update_func(ctx)
 
 
-def read(viewer, layer) -> None:
-    # Nothing to do here (for now)
-    pass
-
-
-def delete(viewer, name) -> None:
-    for idx, l in enumerate(viewer.layers):
-        if l.name == name:
-            viewer.layers.pop(idx)
-
-
-def napari_layer_to_results_layer(napari_layer, results: Results) -> Results:
-    # layer_to_kind = {}  # TODO: better approach...
-    if isinstance(napari_layer, napari.layers.Image):
-        kind = "image"
-        data = napari_layer.data
-    elif isinstance(napari_layer, napari.layers.Labels):
-        kind = "mask"
-        data = napari_layer.data
-    elif isinstance(napari_layer, napari.layers.Points):
-        kind = "points"
-        data = napari_layer.data
-    elif isinstance(napari_layer, napari.layers.Tracks):
-        kind = "tracks"
-        data = napari_layer.data
-    elif isinstance(napari_layer, napari.layers.Vectors):
-        kind = "vectors"
-        data = napari_layer.data
-    elif isinstance(napari_layer, napari.layers.Shapes):
-        # TODO: For now, when a `Shapes` layer is created, we assume it's meant to contain boxes (rectangles).
-        # So, it won't work with algorithms that would use annotated "Paths" as input (quite rare).
-        kind = "boxes"
-        data = None  # instead of []
-    else:
-        print("Could not convert this layer: ", napari_layer)
-        return results
-
-    results.create(kind=kind, data=data, name=napari_layer.name)
-    
-    return results
-
-
-class NapariResults(LayerStackBase):
+class NapariResults(Results):
     """Works like Results, but behaves in sync with a Napari Viewer."""
 
     def __init__(
         self,
         viewer: Optional[napari.Viewer] = None,
         pbar: Optional[QProgressBar] = None,
+        layers: Optional[List[DataLayer]] = None,
     ):
+        super().__init__(layers=layers)
+        
         # Create a Viewer
         if viewer is None:
             self.viewer = napari.Viewer()
@@ -193,9 +92,6 @@ class NapariResults(LayerStackBase):
         else:
             self.pbar = pbar
 
-        # Create a Results object
-        self.results = Results()
-
         # Instanciate layers and add the existing Napari viewer layers to results
         for l in self.viewer.layers:
             self._handle_new_layer(l)
@@ -204,75 +100,6 @@ class NapariResults(LayerStackBase):
         self.connect_layer_added_event(self.sync_layer_added)
         self.connect_layer_removed_event(self.sync_layer_removed)
         self.connect_layer_renamed_event(self.sync_layer_renamed)
-
-    def sync_layer_added(self, e):
-        added_napari_layer = e.source[-1]
-        self._handle_new_layer(added_napari_layer)
-
-    def sync_layer_renamed(self, e):
-        viewer_layer_names = [l.name for l in self.viewer.layers]
-        new_name = e.source
-        for layer in self.results:
-            if layer.name not in viewer_layer_names:
-                layer.name = new_name
-
-    def sync_layer_removed(self, e):
-        layer_name = e.value.name
-        self.delete(layer_name)
-
-    def _handle_new_layer(self, napari_layer):
-        existing_layer = self.read(napari_layer.name)
-        if existing_layer is None:
-            self.results = napari_layer_to_results_layer(napari_layer, self.results)
-
-    @property
-    def layers(self):
-        return self.results.layers
-
-    @property
-    def pixel_domain(self) -> List[int]:
-        return self.results.pixel_domain
-
-    def __iter__(self):
-        return iter(self.results.layers)
-
-    def __getitem__(self, idx):
-        return self.results.layers[idx]
-
-    def create(self, kind, name=None, **kwargs) -> DataLayer:
-        layer = self.results.create(kind=kind, name=name, **kwargs)
-        create(self.viewer, layer)
-        return layer
-
-    def read(self, name: str) -> Optional[DataLayer]:
-        layer = self.results.read(name)
-        read(self.viewer, layer)
-        return layer
-
-    def merge(self, layer_stack: Optional[LayerStackBase]) -> None:
-        if layer_stack is None:
-            return
-
-        dst_layers = []
-        for src_layer in layer_stack:
-            dst_layer = self.read(src_layer.name)
-            if dst_layer is None:
-                dst_layer = self.create(
-                    kind=src_layer.kind,
-                    name=src_layer.name,
-                    meta=src_layer.meta,
-                    merger=src_layer.merger_type,
-                    data_serializer=src_layer.data_serializer_type,
-                )
-            dst_layers.append(dst_layer)
-
-        for src_layer, dst_layer in zip(layer_stack, dst_layers):
-            dst_layer.merge(src_layer)
-            update(self.viewer, dst_layer, self.pbar)
-
-    def delete(self, name) -> None:
-        self.results.delete(name)
-        delete(self.viewer, name)
 
     def connect_layer_renamed_event(self, func: Callable):
         self.viewer.layers.events.inserted.connect(
@@ -284,3 +111,120 @@ class NapariResults(LayerStackBase):
 
     def connect_layer_removed_event(self, func: Callable):
         self.viewer.layers.events.removed.connect(func)
+
+    def sync_layer_added(self, e):
+        added_napari_layer = e.source[-1]
+        self._handle_new_layer(added_napari_layer)
+
+    def sync_layer_renamed(self, e):
+        viewer_layer_names = [l.name for l in self.viewer.layers]
+        new_name = e.source
+        for layer in self.layers:
+            if layer.name not in viewer_layer_names:
+                layer.name = new_name
+
+    def sync_layer_removed(self, e):
+        layer_name = e.value.name
+        self.delete(layer_name)
+
+    def _handle_new_layer(self, napari_layer):
+        existing_layer = self.read(napari_layer.name)
+        if existing_layer is not None:
+            return
+            # self.results = napari_layer_to_results_layer(napari_layer, self.results)
+        # layer_to_kind = {}  # TODO: better approach...
+        if isinstance(napari_layer, napari.layers.Image):
+            kind = "image"
+            data = napari_layer.data
+        elif isinstance(napari_layer, napari.layers.Labels):
+            kind = "mask"
+            data = napari_layer.data
+        elif isinstance(napari_layer, napari.layers.Points):
+            kind = "points"
+            data = napari_layer.data
+        elif isinstance(napari_layer, napari.layers.Tracks):
+            kind = "tracks"
+            data = napari_layer.data
+        elif isinstance(napari_layer, napari.layers.Vectors):
+            kind = "vectors"
+            data = napari_layer.data
+        elif isinstance(napari_layer, napari.layers.Shapes):
+            # TODO: For now, when a `Shapes` layer is created, we assume it's meant to contain boxes (rectangles).
+            # So, it won't work with algorithms that would use annotated "Paths" as input (quite rare).
+            kind = "boxes"
+            data = None  # instead of []
+        else:
+            print("Could not convert this layer: ", napari_layer)
+            return
+
+        # Keep track of the new Napari layer in the layer stack (even without any layer metadata)
+        self.create(kind=kind, name=napari_layer.name, data=data)
+            
+    def post_create(self, layer: DataLayer) -> DataLayer:
+        if layer.data is None:
+            return layer
+        
+        kind = layer.kind
+        data = layer.data
+        name = layer.name
+        meta = layer.meta
+        
+        napari_layer = None
+        if kind == "image":
+            napari_layer = self.viewer.add_image(data, name=name)
+        elif kind == "mask":
+            napari_layer = self.viewer.add_labels(data.astype(np.uint16), name=name)
+        elif kind == "points":
+            napari_layer = self.viewer.add_points(data, name=name)
+        elif kind in ["boxes", "paths"]:
+            if "shape_type" in meta:  # Make sure it isn't used twice
+                meta.pop("shape_type")
+            if kind == "boxes":
+                napari_layer = self.viewer.add_shapes(data, name=name, shape_type="rectangle")
+            elif kind == "paths":
+                napari_layer = self.viewer.add_shapes(data, name=name, shape_type="path")
+        elif kind == "vectors":
+            napari_layer = self.viewer.add_vectors(data, name=name)
+        elif kind == "tracks":
+            napari_layer = self.viewer.add_tracks(data, name=name)
+
+        if napari_layer is not None:
+            _set_layer_attributes_from_meta(meta, napari_layer)
+        
+        return layer
+
+    def post_delete(self, name: str) -> None:
+        """Hook called after layer deletion."""
+        for idx, l in enumerate(self.viewer.layers):
+            if l.name == name:
+                self.viewer.layers.pop(idx)
+
+    def post_merge(self, dst_layers: List[DataLayer]) -> None:
+        for layer in dst_layers:
+            update_hooks = {
+                "image": _napari_layer_update,
+                "mask": _napari_layer_update,
+                "points": _napari_layer_update,
+                "boxes": _napari_layer_update,
+                "paths": _napari_layer_update,
+                "vectors": _napari_layer_update,
+                "tracks": _napari_layer_update,
+                "notification": _notification_update,
+                "float": _textlayer_update,
+                "int": _textlayer_update,
+                "bool": _textlayer_update,
+                "str": _textlayer_update,
+                "choice": _textlayer_update,
+                "progress": _pbar_update,
+            }            
+            update_func: Optional[Callable] = update_hooks.get(layer.kind)
+            
+            # Before doint the napari layer update, make sure the viewer layers exist by running post_create..
+            # A little weird, but for now the only solution that seems to work!
+            if update_func is _napari_layer_update:
+                if not layer.name in [l.name for l in self.viewer.layers]:
+                    self.post_create(layer)
+            
+            if update_func is not None:
+                ctx = UpdateContext(viewer=self.viewer, layer=layer, pbar=self.pbar)
+                update_func(ctx)
--- src/napari_serverkit/widgets/parameter_panel.py
+++ src/napari_serverkit/widgets/parameter_panel.py
@@ -15,7 +15,7 @@ from qtpy.QtWidgets import (
     QWidget,
 )
 
-from napari_serverkit.widgets.napari_results import NapariResults, update
+from napari_serverkit.widgets.napari_results import NapariResults
 
 NAPARI_LAYER_MAPPINGS: Dict[str, Type[napari.layers.Layer]] = {
     "image": napari.layers.Image,
@@ -162,55 +162,54 @@ class ParameterPanel:
                 cb.clear()
                 for layer in self.napari_results.viewer.layers:
                     if isinstance(layer, layer_type):
-                        cb.addItem(layer.name, layer.data)
+                        
+                        # Napari layers data are not always in the format expected by serverkit, so we do the conversion here
+                        # and assign serverkit-formatted data to the combobox data attributes, which get retreived later as parameters
+                        
+                        # For boxes, extract the rectangle data from shapes layers (and convert them to Numpy)
+                        if kind == "boxes":
+                            data = None
+                            if isinstance(layer.data, list):
+                                if len(layer.data) > 0:
+                                    rectangle_data = []
+                                    for d, t in zip(layer.data, layer.shape_type):
+                                        if t == "rectangle":
+                                            rectangle_data.append(d)
+                                    if len(rectangle_data) > 0:
+                                        data = np.array(rectangle_data)
+                            cb.addItem(layer.name, data)
+                        
+                        # For paths, extract the path data from shapes layers
+                        elif kind == "paths":
+                            data = None
+                            if isinstance(layer.data, list):
+                                if len(layer.data) > 0:
+                                    path_data = []
+                                    for d, t in zip(layer.data, layer.shape_type):
+                                        if t == "rectangle":
+                                            path_data.append(d)
+                                    if len(path_data) > 0:
+                                        data = path_data
+                            cb.addItem(layer.name, data)
+                        
+                        else:                      
+                            cb.addItem(layer.name, layer.data)
 
     def get_algo_params(self) -> Dict:
         """Create a dictionary representation of parameter values based on the UI state."""
         algo_params = {}
-
         for name, state_item in self.ui_state.items():
             if state_item.param_type in NAPARI_LAYER_MAPPINGS:
                 if state_item.qt_widget.currentText():
-                    layer_name = state_item.qt_widget.currentText()
-                    layer = self.napari_results.viewer.layers[layer_name]
-                    data = layer.data if layer else None
-                    # For Images and Masks, the results_layer.data is the napari layer's data (remains true when a mask is annotated)
-                    # However, this is not the case for shapes (points, vectors, rectangles...).
-                    # In this case, we need to manually sync results_layer.data with the current napari layer data here.
-                    # This is not great (probably) - TODO: find a better way of handling this?
-                    results_layer = self.napari_results.read(layer_name)
-                    if results_layer is not None:
-                        if results_layer.data is not data:
-                            # Moreover, we need to handle boxes as a special case; Shapes.data are interpreted as boxes,
-                            # however the napari layer data is a list of arrays, so we need to cast it into a numpy array of shape (N, 4, D).
-                            if results_layer.kind == "boxes":
-                                if isinstance(data, list):
-                                    if len(data) == 0:
-                                        # If the layer data is an empty list, we should convert it to None instead.
-                                        data = None
-                                    else:
-                                        # We assume the Shapes layer contains rectangles that can be casted to a single "boxes" array.
-                                        try:
-                                            data = np.asarray(data)
-                                        except:
-                                            print(
-                                                "Could not interpret the content of this Shapes layer as boxes (ignoring it instead): ",
-                                                layer,
-                                            )
-                                            data = None
-
-                            update(
-                                viewer=self.napari_results.viewer,
-                                layer=results_layer,
-                                pbar=self.napari_results.pbar,
-                            )
+                    data = state_item.qt_widget.currentData()
+                    # layer_name = state_item.qt_widget.currentText()
+                    # layer = self.napari_results.viewer.layers[layer_name]
+                    # data = layer.data if layer else None               
                 else:
                     data = None
             else:
                 data = state_item.widget_value_recover_func(state_item.qt_widget)
-
             algo_params[name] = data
-
         return algo_params
 
     def manage_cbs_events(self, worker):
