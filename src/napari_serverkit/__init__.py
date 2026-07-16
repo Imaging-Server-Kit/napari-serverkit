13de3dbc5c39e10d3aefb0588f456770276d38ee
Mallory Wittwer <mallory.wittwer@epfl.ch>
2026-07-16 17:19:12 +0200

    Add demos and tools as contributions

--- src/napari_serverkit/__init__.py
+++ src/napari_serverkit/__init__.py
@@ -7,8 +7,25 @@ try:
 except ImportError:
     __version__ = "unknown"
 
+from qtpy.QtWidgets import QWidget
+
 from napari_serverkit.widgets import AlgorithmWidget, ServerKitHttpWidget, NapariStack
+import imaging_server_kit as sk
 from imaging_server_kit import Algorithm
+
+
+class AlgorithmToolsWidget(QWidget):
+    def __init__(self, viewer: "napari.viewer.Viewer"):
+        super().__init__()
+        widget = sk.to_qwidget(sk.tools, viewer=viewer)
+        self.setLayout(widget.layout())
+
+
+class AlgorithmDemosWidget(QWidget):
+    def __init__(self, viewer: "napari.viewer.Viewer"):
+        super().__init__()
+        widget = sk.to_qwidget(sk.demos, viewer=viewer)
+        self.setLayout(widget.layout())
 
 
 def add_as_widget(
